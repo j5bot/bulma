2a24850bfe57a8224e28ad57abde0f093e42ef85
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2016-10-10 11:44:08 -0400

    moved to build folder structure

--- build-scripts/make-attribute-selector.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "processing: $1"
-
-sed -i '' 's#\.\([a-z][^\:,.)]*\)#[data-am-bulma~="\1"]#g' "$1"
-sed -i '' 's#\[data-am-bulma~="sass""\]#.sass"#g' "$1"
--- build-scripts/make-attribute-selectors.sh
+++ build-scripts/make-attribute-selectors.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-rm -rf sass-attribute-selectors
+rm -rf src/sass-attribute-selectors dist/sass-attribute-selectors
 
-cp -R sass sass-attribute-selectors
+cp -R src/sass src/sass-attribute-selectors
 
-pushd sass-attribute-selectors
+pushd src/sass-attribute-selectors
 
-grep -r '^\s*\.' -l --null .
 grep -r '^\s*\.' -l --null . | xargs -0 sed -i '' 's#\.\([a-z][^\:,.)]*\)#[data-am-bulma~="\1"]#g'
 grep -r '^\s*&\.' -l --null . | xargs -0 sed -i '' 's#\.\([a-z][^\:,.)]*\)#[data-am-bulma~="\1"]#g'
-grep -r '\[data-am-bulma~="sass""\]' -l --null . | xargs -0 sed -i '' 's#\[data-am-bulma~="sass""\]#.sass"#g'
 
+grep -r '\[data-am-bulma~="sass""\]' -l --null . | xargs -0 sed -i '' 's#\[data-am-bulma~="sass""\]#.sass"#g'
 
 popd
 
-cp bulma.sass bulma-attribute-selectors.sass
+cp src/bulma.sass src/bulma-attribute-selectors.sass
 
-sed -i '' 's#sass/#sass-attribute-selectors/#g' bulma-attribute-selectors.sass
+sed -i '' 's#sass/#sass-attribute-selectors/#g' src/bulma-attribute-selectors.sass
 
-echo '/* @import "bulma.sass" */' >> bulma-attribute-selectors.sass
+echo '/* @import "bulma.sass" */' >> src/bulma-attribute-selectors.sass
 
-echo 'Recreated bulma-attribute-selectors.sass at: ' $(date) >> bulma-attribute-selectors.log
+echo 'Recreated bulma-attribute-selectors.sass at: ' $(date) >> logs/bulma-attribute-selectors.log
+
+mkdir -p dist/bulma/css
+mkdir -p dist/bulma-attribute-selectors/css
