4456a55ca6766b3d4826477848e4e3b050c4ecf2
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2017-06-20 00:47:36 -0400

    moving, deleting, shuffling

--- build-scripts/make-attribute-selectors.sh
+++ build-scripts/make-attribute-selectors.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm -rf src/sass-attribute-selectors dist/sass-attribute-selectors
+rm -rf sass-attribute-selectors
 
-cp -R src/sass src/sass-attribute-selectors
+cp -R sass sass-attribute-selectors
 
-pushd src/sass-attribute-selectors
+pushd sass-attribute-selectors
 
 grep -r '^\s*\.' -l --null . | xargs -0 sed -i 's#\.\([a-z][^\:,.)]*\)#[data-am-bulma~="\1"]#g'
 grep -r '^\s*&\.' -l --null . | xargs -0 sed -i 's#\.\([a-z][^\:,.)]*\)#[data-am-bulma~="\1"]#g'
@@ -13,14 +13,14 @@ grep -r '\[data-am-bulma~="sass""\]' -l --null . | xargs -0 sed -i 's#\[data-am-
 
 popd
 
-cp src/bulma.sass src/bulma-attribute-selectors.sass
+cp bulma.sass bulma-attribute-selectors.sass
 
-sed -i 's#sass/#sass-attribute-selectors/#g' src/bulma-attribute-selectors.sass
+sed -i 's#sass/#sass-attribute-selectors/#g' bulma-attribute-selectors.sass
 
-echo '/* @import "bulma.sass" */' >> src/bulma-attribute-selectors.sass
+echo '/* @import "bulma.sass" */' >> bulma-attribute-selectors.sass
 
 mkdir -p logs
 echo 'Recreated bulma-attribute-selectors.sass at: ' $(date) >> logs/bulma-attribute-selectors.log
 
-mkdir -p dist/bulma/css
-mkdir -p dist/bulma-attribute-selectors/css
+# mkdir -p dist/bulma/css
+# mkdir -p dist/bulma-attribute-selectors/css
